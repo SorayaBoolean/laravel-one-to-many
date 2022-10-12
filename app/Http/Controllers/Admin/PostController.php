5538c78fb3ef9c71219f270cdcf85f15502349a5
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-10-13 00:09:39 +0200

    add category select to create view and function

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Category;
 use App\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
@@ -27,7 +28,8 @@ class PostController extends Controller
      */
     public function create()
     {
-        return view('admin.posts.create');
+        $categories = Category::all();
+        return view('admin.posts.create', compact('categories'));
     }
 
     /**
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -6,6 +6,18 @@
 
         @csrf
         <h1>Create your post:</h1>
+
+        <div class=" form-group mb-3">
+            <label for="category_id" class="form-label">Category</label>
+            <select class="form-control" name="category_id" id="category_id">
+                <option value="">no category</option>
+
+                @foreach($categories as $category)
+                <option value="{{$category->id}}">{{$category->name}}</option>
+                @endforeach
+            </select>
+        </div>
+
         <div class="mb-3">
             <label for="title" required max="255" class="form-label">Title</label>
             <input type="text" class="form-control" id="title" name="title">
